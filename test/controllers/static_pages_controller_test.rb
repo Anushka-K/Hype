1721a43b0a5f43bbe10abd3526e4357986ea5a0a
Anushka-K <kulshres@grinnell.edu>
2020-09-20 08:20:33 +0000

    add sign up and manage props views and tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,34 +2,34 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get home_path
     assert_response :success
     assert_select "title", "Home | Hype"
   end
 
   test "should get signin" do
-    get static_pages_signin_url 
+    get signin_path
     assert_response :success
     assert_select "title", "Sign In | Hype"
   end
   test "should get signup" do
-    get static_pages_signup_url 
+    get signup_path 
     assert_response :success
     assert_select "title", "Sign Up | Hype"
   end
   test "should get addprops" do
-    get static_pages_addprops_url
+    get addprops_path
     assert_response :success
     assert_select "title", "Add Props | Hype"
   end
   test "should get about" do
-    get static_pages_about_url
+    get root_path
     assert_response :success
     assert_select "title", "Hype"
   end
   
   test "should get manageprops" do
-    get static_pages_manageprops_url
+    get manageprops_path
     assert_response :success
     assert_select "title", "Manage Props | Hype"
   end
